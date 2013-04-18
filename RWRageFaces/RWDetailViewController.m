73c1adcace94d5797798dcd6066f95401e3c53ff
Pietro Rea <pietrorea@gmail.com>
2013-04-17 23:46:07 -0400

    Added sharing UIActivityViewController to RWDetailViewController

--- RWRageFaces/RWDetailViewController.m
+++ RWRageFaces/RWDetailViewController.m
@@ -13,6 +13,9 @@
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *doneButton;
 @property (strong, nonatomic) IBOutlet UINavigationBar *navigationBar;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *shareButton;
+
+@property (strong, nonatomic) UIPopoverController* popOver;
 
 @end
 
@@ -44,7 +47,35 @@
 
 - (IBAction)doneButtonTapped:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+    
+    if ([self.popOver isPopoverVisible]) {
+        [self.popOver dismissPopoverAnimated:YES];
+    }
 }
 
+- (IBAction)shareButtonTapped:(id)sender {
+    
+    /* Set up UIActivityViewController */
+    
+    NSURL* url = [NSURL URLWithString:@"http://www.raywenderlich.com"];
+    NSArray* activityItems = @[self.imageName, self.imageView.image, url];
+    
+    UIActivityViewController* activityViewController = [[UIActivityViewController alloc] initWithActivityItems:activityItems
+                                                                                         applicationActivities:nil];
+    
+    activityViewController.excludedActivityTypes = @[UIActivityTypeAssignToContact, UIActivityTypePrint, UIActivityTypeMessage];
+    
+    [activityViewController setCompletionHandler:^(NSString* activityType, BOOL completed) {
+        
+    }];
+    
+    /* Present UIActivityViewController in popover */
+    
+    self.popOver = [[UIPopoverController alloc] initWithContentViewController:activityViewController];
+    
+    [self.popOver presentPopoverFromBarButtonItem:self.shareButton
+                         permittedArrowDirections:UIPopoverArrowDirectionAny
+                                         animated:YES];
+}
 
 @end
